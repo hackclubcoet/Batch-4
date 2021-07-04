a531cad9c5969191b7c1b91048497d96d190b52a
Kenzamanoly <86522555+Kenzamanoly@users.noreply.github.com>
2021-07-04 20:49:38 +0530

    Update Kenza.py

--- Kenza.py
+++ Kenza.py
@@ -1,5 +1,6 @@
 import time
 '''We import time module so we can use sleep which delays time.'''
+#kenza-4-Hello World(Animation)
 def result(word):
   '''returns "Hello world" character by character and there should be a delay of 1 sec between printing each character.'''
   for i in word:
