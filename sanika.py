daf1a26a983a3403ed4024e666dccf2c1a8541ab
sanikav <56670471+sanikav@users.noreply.github.com>
2021-07-04 13:06:50 +0530

    Add files via upload

---
+++ sanika.py
@@ -0,0 +1,7 @@
+#python program to print a remainder message after 1 minute
+# importing time package
+import time
+# creating a time delay of 60 seconds
+time.sleep(60)
+#printing "TIMES UP " after delay OF 1 MINUTE
+print("times up")
