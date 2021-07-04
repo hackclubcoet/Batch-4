c75d7cd0d49e584283bd1553b09e32acc513e01d
Saniiahh <86533208+Saniiahh@users.noreply.github.com>
2021-07-04 19:01:19 +0530

    Add files via upload

---
+++ Saniya.py
@@ -0,0 +1,133 @@
+
+
+#Saniyamuhammedsali-B4-chessboard 
+
+import turtle  
+
+   
+# create screen object
+#lets call it sc
+
+sc = turtle.Screen() 
+
+   
+# create turtle object 
+# lets call the turtle as pen
+
+
+pen = turtle.Turtle() 
+
+   
+# method to draw square 
+
+def draw():
+#calls the pen to draw
+
+   
+
+  for i in range(4): 
+  #moves the turtle 30 pixels forward 
+    pen.forward(30) 
+  #moves the turtle left
+    pen.left(90) 
+
+   
+
+  pen.forward(30) 
+
+   
+
+  
+
+     
+# Driver Code 
+
+if __name__ == "__main__" : 
+
+      
+
+    # set screen 
+
+    sc.setup(600, 600) 
+
+       
+
+    # set turtle object speed 
+
+    pen.speed(100) 
+
+       
+
+    # loops for board 
+
+    for i in range(8): 
+
+       
+
+      # not ready to draw 
+
+      pen.up() 
+
+       
+
+      # set position for every row 
+
+      pen.setpos(0, 30 * i) 
+
+       
+
+      # ready to draw 
+
+      pen.down() 
+
+       
+
+      # row 
+
+      for j in range(8): 
+
+       
+
+        # conditions for alternative color 
+
+        if (i + j)% 2 == 0: 
+
+          col ='black'
+
+       
+
+        else: 
+
+          col ='white'
+
+       
+
+        # fill with given color 
+
+        pen.fillcolor(col) 
+
+       
+
+        # start filling with colour 
+
+        pen.begin_fill() 
+
+       
+
+        # call method 
+
+        draw() 
+
+        # stop filling 
+
+        pen.end_fill() 
+
+       
+
+    # hide the turtle 
+
+    pen.hideturtle() 
+
+       
+
+    # This code 
