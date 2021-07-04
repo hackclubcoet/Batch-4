799c000db1132e5a3721df7b42a7346023c1fc97
sanikav <56670471+sanikav@users.noreply.github.com>
2021-07-04 23:17:23 +0530

    Update and rename sanika.py to sanika_v.py

--- sanika.py
+++
@@ -1,8 +0,0 @@
-#python program to print a remainder message after 1 minute
-# importing time package
-#sanika-4-Reminder Message
-import time
-# creating a time delay of 60 seconds
-time.sleep(60)
-#printing "TIMES UP " after delay OF 1 MINUTE
-print("times up")
---
+++ sanika_v.py
@@ -0,0 +1,13 @@
+import time
+'''importing time package'''
+#Sanika_V-4-Reminder Message
+time.sleep(60)
+'''creating a time delay of 60 seconds'''
+print("TIMES UP")
+
+
+
+
+
+
+
